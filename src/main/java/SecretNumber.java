208bc5c5117fb8d161adcc6f13f74965125ef413
Kat Fagg <katjfagg@gmail.com>
2022-08-11 20:57:08 +0100

    completed mvp

---
+++ src/main/java/SecretNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SecretNumber {
+    public static void main(String[] args) {
+
+//        Prompt user to input number
+        System.out.println("Please enter your number guess");
+
+//        Collect user input
+        Scanner reader = new Scanner(System.in); // creates scanner object
+        int userNumber = reader.nextInt(); //initiates variable based on input
+
+//        Secret number variable initialisation
+        int secretNumber = 7; // Would edit code to create random integer if possible
+
+//        If user number is equal to secret number, print "Well Done"
+//        If user number is higher than secret number, print "Too high"
+//        Otherwise print "Too low" (only other option)
+        if (userNumber == secretNumber) {
+            System.out.println("You got it right");
+        } else if (userNumber > secretNumber) {
+            System.out.println("Too high");
+        } else {
+            System.out.println("Too low");
+        }
+    }
+}
